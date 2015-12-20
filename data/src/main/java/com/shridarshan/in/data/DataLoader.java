84712f15bc85b2d37a81558865199f3c9d04605e
srinivaskulkarni1 <srinivaskulkarni1@yahoo.com>
2015-12-20 09:37:31 +0530

    now connects and queries from cassandra instance

--- data/src/main/java/com/shridarshan/in/data/DataLoader.java
+++ data/src/main/java/com/shridarshan/in/data/DataLoader.java
@@ -8,26 +8,58 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.AbstractApplicationContext;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
+import com.datastax.driver.core.policies.DefaultRetryPolicy;
+import com.datastax.driver.core.policies.TokenAwarePolicy;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.shridarshan.in.pojo.PojoFactory;
 import com.shridarshan.in.pojo.Temple;
 import com.shridarshan.in.util.BeanConstants;
 
-public class DataLoader implements ApplicationContextAware{
+public class DataLoader implements ApplicationContextAware {
 
 	private AbstractApplicationContext context;
+	private Cluster cluster;
+	private Session session;
 
 	public List<Temple> getTempleList() {
-		
-		PojoFactory pojoFactory = (PojoFactory) context.getBean(BeanConstants.POJO_FACTORY);
-		Temple temple = pojoFactory.getTemplePojo();
-		temple.setGod("Shri Krishna");
-		temple.setDistrict("Udupi");
-		temple.setPlace("Udupi");
-		temple.setState("Karnataka");
 
+		connect();
+
+		PojoFactory pojoFactory = (PojoFactory) context
+				.getBean(BeanConstants.POJO_FACTORY);
 		List<Temple> templeList = new ArrayList<Temple>();
-		templeList.add(temple);
+
+		Statement select = QueryBuilder.select().all().from("temple");
+		ResultSet results = session.execute(select);
+
+		for (Row row : results) {
+			Temple temple = pojoFactory.getTemplePojo();
+			temple.setGod(row.getString("god"));
+			temple.setDistrict(row.getString("district"));
+			temple.setPlace(row.getString("place"));
+			temple.setState(row.getString("state"));
+			templeList.add(temple);
+		}
 		return templeList;
+
+	}
+
+	@SuppressWarnings("deprecation")
+	private void connect() {
+		cluster = Cluster
+				.builder()
+				.addContactPoint("localhost")
+				.withRetryPolicy(DefaultRetryPolicy.INSTANCE)
+				.withLoadBalancingPolicy(
+						new TokenAwarePolicy(new DCAwareRoundRobinPolicy()))
+				.build();
+		session = cluster.connect("shridarshan");
 
 	}
 
